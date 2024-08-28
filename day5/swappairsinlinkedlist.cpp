54f1f0464ac400c7bab17312ea9e40621123c2d1
Nitin <92179118+Nitinyad@users.noreply.github.com>
2024-08-28 19:36:14 +0530

    Create swappairsinlinkedlist.cpp

---
+++ day5/swappairsinlinkedlist.cpp
@@ -0,0 +1,13 @@
+
+//recursive solutions
+class Solutions{
+  public:
+  ListNode* swapPairs(ListNode* head){
+      if(head == NULL || head -> next == NULL)
+        return head;
+      ListNode* temp = head -> next;
+      head -> next = swapPairs(head -> next -> next );
+      temp -> next = head;
+      return temp;
+  }
+};
