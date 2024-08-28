577914e8ec08cbd8f4a49868ddc48ba350ef087e
Nitin <92179118+Nitinyad@users.noreply.github.com>
2024-08-28 18:31:49 +0530

    Update pathsum.cpp

--- day5/pathsum.cpp
+++ day5/pathsum.cpp
@@ -1,4 +1,5 @@
 
+// using two pointer
 
 class Solution {
 public:
@@ -17,5 +18,43 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         int sum = 0;
         return solve(root , 0 ,targetSum);
+    }
+};
+
+
+//using sliding window
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        int i=0;
+        int j=0;
+        
+        int n1=haystack.size();
+        int n2=needle.size();
+        if(n1<n2)
+        return -1;
+
+        string s="";
+        int ans=-1;
+        while(j<n1)
+        {
+            s+=haystack[j];
+            if(j-i+1==n2)
+            {
+                if(s==needle)
+                {
+                    ans=i;
+                    break;
+                }
+                else
+                {
+                    i++;
+                    s.erase(s.begin()+0);
+                }
+            }
+            j++;
+        }
+        return ans;
     }
 };
