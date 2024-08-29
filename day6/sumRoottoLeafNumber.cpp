bb3e519769fce0d3bd4c89c6b15cf5c8b73ef912
Nitin <92179118+Nitinyad@users.noreply.github.com>
2024-08-29 22:47:12 +0530

    Update sumRoottoLeafNumber.cpp

--- day6/sumRoottoLeafNumber.cpp
+++ day6/sumRoottoLeafNumber.cpp
@@ -14,3 +14,28 @@ public:
         return solve(root , 0);
     }
 };
+
+
+//BACKTRACKING 
+class Solution {
+public:
+    void solve(TreeNode* root , int& num,int &sum ){
+        if(!root){
+            return ;
+        }
+        num = num * 10 + root -> val;
+        if(!root -> left && ! root -> right ){
+            sum +=  num;
+        }
+        solve(root -> left , num ,sum);
+        solve(root -> right , num , sum);
+        num -= root -> val;
+        num /= 10;
+    }
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        int num = 0;
+        solve(root , num , sum);
+        return sum;
+    }
+};
