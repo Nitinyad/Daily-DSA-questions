c7083274d4a28fca2f7336318277b3f719772a7e
Nitin <92179118+Nitinyad@users.noreply.github.com>
2024-09-02 17:41:12 +0530

    implementstackusingqueue.cpp

--- day8/implementstackusingqueue.cpp
+++ day8/implementstackusingqueue.cpp
@@ -1,3 +1,4 @@
+//USING TWO QUEUES 
 class MyStack {
 public:
     queue<int>q1 , q2;  
@@ -33,3 +34,38 @@ public:
         return q1.empty();
     }
 };
+
+
+
+// USING SINGLE QUEUE
+class MyStack {
+private:
+//using single queue
+        queue<int>q1;
+public:
+    MyStack() {
+    }
+    
+    void push(int x) {
+        q1.push(x);
+        for(int i = 0; i < q1.size() -1 ;i++){
+            q1.push(q1.front());
+            q1.pop();
+        }
+    }
+    
+    int pop() {
+        int x = q1.front();
+        q1.pop();
+        return x;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
