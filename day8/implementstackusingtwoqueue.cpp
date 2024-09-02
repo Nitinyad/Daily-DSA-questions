6293fd536a770916e8515016e28558f4dad2b2b7
Nitin <92179118+Nitinyad@users.noreply.github.com>
2024-09-02 16:55:41 +0530

    Create implementstackusingtwoqueue.cpp

---
+++ day8/implementstackusingtwoqueue.cpp
@@ -0,0 +1,35 @@
+class MyStack {
+public:
+    queue<int>q1 , q2;  
+    MyStack() {
+    }
+    
+    void push(int x) {
+        // add into the q2;
+        q2.push(x);
+        // move all element of q1 to q2
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        // swaping the q1 and q2 because the final stack is into the q1;
+        while(!q2.empty()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+    }
+    
+    int pop() {
+        int x = q1.front();
+        q1.pop();
+        return x;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
